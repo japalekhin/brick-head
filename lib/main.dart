f8d71448991ee861e50dc02ac764c52efd720b99
Japa Alekhin Llemos <jap@alekhin.io>
2019-10-20 02:43:48 +0800

    just load game directly to runapp

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,5 @@ import 'package:brick_head/brick-game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: SafeArea(
-          child: BrickGame().widget,
-        ),
-      ),
-    ),
-  );
+  runApp(BrickGame().widget);
 }
